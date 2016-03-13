7ac7d278df0840ede9aa04909a7dffc06864a88b
Rafael Miranda-Esquivel <dmirandae@gmail.com>
2016-03-12 22:56:49 -0500

    Version 0.60-36.02

--- R/Read.data.R
+++ R/Read.data.R
@@ -1,23 +1,22 @@
 #'
 #' @title Read distributions.
 #'
-#' @description Read distributions as a csv with two columns species and area
+#' @description Read distributions as a csv with two columns labeled "species" and "area"
 #'
-#' @param data.File a csv file to read
-#'   
+#' @param data.File a csv file to read, with two columns labeled "species" and "area"
+#'
 #' @return a data.frame object with the distribution by species 
 #'
 #'@author Miranda-Esquivel Daniel R.
 #'
 #'
 
-
-
-
 Read.Data <-
 function (data.File) {
     
-  initial.Distribution <- read.csv(data.File,header=T,sep=" ")
+    initial.Distribution <- read.csv(data.File,header=T,sep=" ")
+
+    if (all(colnames(initial.Distribution)==c("species","area"))){
   
   final.Distribution1 <- table(initial.Distribution$species,initial.Distribution$area)
   
@@ -25,6 +24,9 @@ function (data.File) {
   
   final.Distribution2$species <- levels(as.factor(initial.Distribution$species))
   
-  return(final.Distribution2)
+        return(final.Distribution2)
+    }else
+    {stop("wrong format in data file")}
 
 }
+
